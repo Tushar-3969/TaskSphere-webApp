dd9b3de306466654f1839bc88b9f1005e701ccf9
Tushar Pathare <patharetushar10@gmail.com>
2025-08-19 00:14:48 +0530

    add file

---
+++ mini-project/src/components/TaskList/TaskList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id="tasklist" className='h-[55%] overflow-x-auto w-full flex items-center justify-flex-start gap-5 flex-nowrap py-5 mt-10'>
+        <div className='flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl'>
+            <h2></h2>
+        </div>
+    </div>
+  )
+}
+
+export default TaskList
